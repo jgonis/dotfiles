03e199d6b7a99ba7ad842dddb30047ec13d2b0fa
Jeff Gonis <jeff.gonis@gmail.com>
2024-03-29 18:00:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,3 +22,9 @@ $BINDIR/chezmoi init $GITHUB_USERNAME
 
 echo "[APPLY] apply dotfiles"
 $BINDIR/chezmoi apply
+
+echo "Install lazygit"
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+cp lazygit $BINDIR/lazygit
